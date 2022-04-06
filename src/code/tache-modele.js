f4b34e4e16cb19df3d6e80180382f68d90bf1b18
Gabriel Dev <gab@gab.dev>
2022-04-06 12:14:10 -0400

    Ajustement du code sur tache-modele.js

--- src/code/tache-modele.js
+++ src/code/tache-modele.js
@@ -6,19 +6,6 @@ export function ajouterTache(e){
     console.log(e.target.elements[0].value);
     const [Taches, setTaches] = useState([]);
 
-    // const obtenirTaches=async()=>{
-    //     const reponse=bdd.collection('taches');
-    //     const donnees=await reponse.get();
-    //
-    //     donnees.docs.forEach(item=>{
-    //         setTaches([...Taches,item.data()])
-    //     })
-    // }
-    //
-    // useEffect(() => {
-    //     obtenirTaches();
-    // }, [])
-
     // Obtenir les taches de la base de donnes firebase firestore
     bdFirestore.collection("taches").get().then(function(querySnapshot) {
         querySnapshot.forEach(function(doc) {
